2287b1a8c66b7851ff3f9ef61259f18ca90471ef
Pavel Chipiga <pavel.chipiga@gmail.com>
2010-08-23 01:29:29 +0300

    Create helper for direct link to post in collection

--- lib/forum_engine/helpers/posts_helper.rb
+++ lib/forum_engine/helpers/posts_helper.rb
@@ -7,6 +7,11 @@ module ForumEngine
       end
 
       module InstanceMethods
+        def collection_link(post = nil)
+          post ||= Post.where(:topic_id => resource.topic_id).last
+          page = (Post.where("topic_id = ? AND id <= ?", post.topic_id, post.id).count.to_f / ForumEngine.per_page).ceil
+          posts_collection_path(post.topic, :page => page, :anchor => post.id)
+        end
       end
 
       module ClassMethods
